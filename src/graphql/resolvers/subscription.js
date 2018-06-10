beff9c5d7ff226c8dfa6588227aac20dd1c1cb92
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-06-10 08:34:25 +0200

    remove useless log

--- src/graphql/resolvers/subscription.js
+++ src/graphql/resolvers/subscription.js
@@ -6,12 +6,6 @@ const UserCollection = 'users';
 
 const SEVEN_DAYS_MILLISECONDS = 604800000;
 const FIVE_MINUTES_MILLISECONDS = 300000;
-// const licenceCollection = 'licences';
-
-// const licenceCount = mongo.count(databaseName, licenceCollection);
-//   const licencePayingCount = mongo.count(databaseName, licenceCollection, {
-//     planId: { $ne: null },
-//   });
 
 const userCount = mongo.count(databaseName, UserCollection, { status: 'ACTIVE' });
 const managerCount = mongo.count(databaseName, UserCollection, { status: 'ACTIVE', role: 'manager' });
