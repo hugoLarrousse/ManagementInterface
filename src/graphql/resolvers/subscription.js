59f080327a9692b7c49f56324cf2468529932a0f
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-06-11 11:35:07 +0200

    get active users by role and team_id + strongify ObjectID

--- src/graphql/resolvers/subscription.js
+++ src/graphql/resolvers/subscription.js
@@ -7,9 +7,9 @@ const UserCollection = 'users';
 const SEVEN_DAYS_MILLISECONDS = 604800000;
 const FIVE_MINUTES_MILLISECONDS = 300000;
 
-const userCount = mongo.count(databaseName, UserCollection, { status: 'ACTIVE' });
+const userCount = mongo.count(databaseName, UserCollection, { status: 'ACTIVE', team_id: { $ne: null } });
 const managerCount = mongo.count(databaseName, UserCollection, { status: 'ACTIVE', role: 'manager' });
-const users = mongo.find(databaseName, UserCollection, { status: 'ACTIVE' });
+const users = mongo.find(databaseName, UserCollection, { status: 'ACTIVE', team_id: { $ne: null } });
 
 
 exports.count = async () => {
@@ -19,18 +19,18 @@ exports.count = async () => {
   ]);
   const usersUnpromising = await users;
   const recentUsers = usersUnpromising.filter(user =>
-    user.last_connected > Date.now() - SEVEN_DAYS_MILLISECONDS).map(user => user.team_id);
+    user.last_connected > Date.now() - SEVEN_DAYS_MILLISECONDS).map(user => String(user.team_id));
 
   const liveUsersCount = usersUnpromising.filter(user =>
     user.last_connected > Date.now() - FIVE_MINUTES_MILLISECONDS).length;
 
-  const teamCount = new Set(usersUnpromising.map(user => user.team_id)).size;
+  const teamCount = new Set(usersUnpromising.map(user => String(user.team_id))).size;
   return {
     userCount,
     managerCount,
     teamCount,
     activeTeamPercentage: recentUsers.length > 0
-      ? (new Set(recentUsers.map(user => user.team_id)).size / teamCount) * 100
+      ? ((new Set(recentUsers).size / teamCount) * 100).toFixed(2)
       : 0,
     liveUsersCount,
   };
