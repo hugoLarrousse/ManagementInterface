56d0cfba868c92a013c5d75129ab3d939072eea5
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-08-09 11:56:51 +0200

    refactoring pipedrive

--- src/dataTest/Utils/pipedrive.js
+++ src/dataTest/Utils/pipedrive.js
@@ -1,18 +1,34 @@
 const requestretry = require('requestretry');
 
 const urlPipedrive = process.env.pipedriveUrl;
+const urlPipedriveOauth = process.env.pipedriveUrlOauth;
 
-const get = async (path, apiToken) => {
+// const apiToken = '8a248db22705fa85b9f25dbe82572d06c2b16aa6'; // Token prod pipedrive samy
+// const apiToken = '5e271bab709f9e74189421d4cc49a60110a312e1'; // Token prod pipedrive Baptiste
+// const apiToken = 'a86eb36af4331b509045bd3ffe209b2c699e439d'; // Token Test Pipedrive
+
+const get = async (path, apiToken, oAuth) => {
   try {
-    const urlApiToken = `&api_token=${apiToken}`;
+    const urlApiToken = oAuth ? '' : `&api_token=${apiToken}`;
     const options = {
-      uri: `${urlPipedrive}${path}${urlApiToken}`,
+      uri: `${oAuth ? urlPipedriveOauth : urlPipedrive}${path}${urlApiToken}`,
       method: 'GET',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
       },
       json: true,
     };
+
+    console.log('options :', options);
+
+    if (oAuth) {
+      Object.assign(options, {
+        headers: {
+          ...options.headers,
+          Authorization: `Bearer ${apiToken}`,
+        },
+      });
+    }
 
     const { error, body } = await requestretry(options);
 
--- src/dataTest/services/pipedrive/pipedrive.js
+++ src/dataTest/services/pipedrive/pipedrive.js
@@ -1,17 +1,15 @@
-const _ = require('lodash');
-
-const {
-  get
-} = require('../../Utils/pipedrive');
+const { get } = require('../../Utils/pipedrive');
 const dates = require('../../Utils/dates');
 
-const getDealsOpenedTimeline = async (apiToken, since, interval = 'month') => {
+const getDealsOpenedTimeline = async (apiToken, since, interval = 'month', isOauth) => {
+  console.log('apiToken :', apiToken);
   try {
     const date = dates.formatDateStartMonth(since);
 
-    const path = `/v1/deals/timeline?start_date=${date}&limit=500&interval=${interval}&amount=1&field_key=add_time`;
+    const path = `${isOauth ? '' : '/v1'}/deals/timeline?start_date=${date}&limit=500&interval=${interval}&amount=1&field_key=add_time`;
 
-    const result = await get(path, apiToken);
+    const result = await get(path, apiToken, isOauth);
+    console.log('result :', result);
     return result.data;
   } catch (e) {
     throw new Error(`${__filename}
@@ -20,12 +18,12 @@ const getDealsOpenedTimeline = async (apiToken, since, interval = 'month') => {
   }
 };
 
-const getDealsWonTimeline = async (apiToken, since, interval = 'month') => {
+const getDealsWonTimeline = async (apiToken, since, interval = 'month', isOauth) => {
   try {
     const date = dates.formatDateStartMonth(since);
-    const path = `/v1/deals/timeline?start_date=${date}&limit=500&interval=${interval}&amount=1&field_key=won_time`;
+    const path = `${isOauth ? '' : '/v1'}/deals/timeline?start_date=${date}&limit=500&interval=${interval}&amount=1&field_key=won_time`;
 
-    const result = await get(path, apiToken);
+    const result = await get(path, apiToken, isOauth);
     return result.data;
   } catch (e) {
     throw new Error(`${__filename}
@@ -34,18 +32,18 @@ const getDealsWonTimeline = async (apiToken, since, interval = 'month') => {
   }
 };
 
-const getAddActivities = async (type, apiToken, since) => {
+const getAddActivities = async (type, apiToken, since, isOauth) => {
   try {
     const compareStartDate = new Date(since);
     let hasMore = false;
     const activities = [];
 
-    let path = `/v1/activities?user_id=0&limit=500&type=${type}&start=0&sort=add_time%20DESC`;
+    let path = `${isOauth ? '' : '/v1'}/activities?user_id=0&limit=500&type=${type}&start=0&sort=add_time%20DESC`;
 
     do {
-      const result = await get(path, apiToken);
+      const result = await get(path, apiToken, isOauth);
       hasMore = result.additional_data.pagination.more_items_in_collection;
-      result.data.forEach(activity => {
+      result.data.forEach(activity => { //eslint-disable-line
         const activityAddTime = new Date(activity.add_time);
         if (activityAddTime > compareStartDate) {
           activities.push(activity);
@@ -53,7 +51,7 @@ const getAddActivities = async (type, apiToken, since) => {
           hasMore = false;
         }
       });
-      path = `/v1/activities?user_id=0&limit=500&type=${type}&start=${result.additional_data.pagination.next_start}&sort=add_time%20DESC`;
+      path = `${isOauth ? '' : '/v1'}/activities?user_id=0&limit=500&type=${type}&start=${result.additional_data.pagination.next_start}&sort=add_time%20DESC`; //eslint-disable-line
     } while (hasMore);
 
     return activities;
@@ -64,18 +62,18 @@ const getAddActivities = async (type, apiToken, since) => {
   }
 };
 
-const getDoneActivities = async (type, apiToken, since) => {
+const getDoneActivities = async (type, apiToken, since, isOauth) => {
   try {
     const compareStartDate = new Date(since);
     let hasMore = false;
     const activities = [];
 
-    let path = `/v1/activities?user_id=0&limit=500&type=${type}&start=0&sort=due_date%20DESC`;
+    let path = `${isOauth ? '' : '/v1'}/activities?user_id=0&limit=500&type=${type}&start=0&sort=due_date%20DESC`;
 
     do {
-      const result = await get(path, apiToken);
+      const result = await get(path, apiToken, isOauth);
       hasMore = result.additional_data.pagination.more_items_in_collection;
-      result.data.forEach(activity => {
+      result.data.forEach(activity => { //eslint-disable-line
         const activityDueTime = new Date(activity.due_date);
         if (activityDueTime > compareStartDate) {
           activities.push(activity);
@@ -83,7 +81,7 @@ const getDoneActivities = async (type, apiToken, since) => {
           hasMore = false;
         }
       });
-      path = `/v1/activities?user_id=0&limit=500&type=${type}&start=${result.additional_data.pagination.next_start}&sort=due_date%20DESC`;
+      path = `${isOauth ? '' : '/v1'}/activities?user_id=0&limit=500&type=${type}&start=${result.additional_data.pagination.next_start}&sort=due_date%20DESC`; //eslint-disable-line
     } while (hasMore);
 
     return activities;
