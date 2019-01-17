38f66e824efbf164d9cac3c5788bfce687fac4e3
Hugo Larrousse <hugo.larrousse@gmail.com>
2019-01-17 13:19:31 +0100

    add log

--- src/dataTest/services/heptaward/sync.js
+++ src/dataTest/services/heptaward/sync.js
@@ -4,6 +4,7 @@ const config = require('config');
 
 const { fixedToken } = process.env;
 console.log('fixedToken', fixedToken);
+console.log('coreUrl', config.get('coreUrl'));
 
 module.exports = (id, crm, email) => {
   console.log('email SYNC', email);
