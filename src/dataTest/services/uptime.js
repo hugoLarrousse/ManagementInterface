7d5812dcf9862b67ef5ff783fe8b83b906abeb13
Hugo Larrousse <hugo.larrousse@gmail.com>
2020-11-20 10:48:31 +0100

    fixed number digit

--- src/dataTest/services/uptime.js
+++ src/dataTest/services/uptime.js
@@ -14,7 +14,7 @@ exports.elapseTime = async () => {
   const originUrl = await mongo.findOne('heptaward', 'counters', { _id: `uptime-elapse-time-${moment().format('DD-MM-YYYY')}` }); //eslint-disable-line
   if (originUrl) {
     const { timer } = originUrl;
-    const avgTime = timer.reduce((a, b) => a + b, 0) / timer.length;
+    const avgTime = (timer.reduce((a, b) => a + b, 0) / timer.length).toFixed(2);
     logger.info(`average elapse time: ${Math.round(avgTime)}ms`);
   }
 };
